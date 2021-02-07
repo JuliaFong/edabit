63f8586ddc17076aa8c9621d59ea63ef1a7d3843
Julia Fong <Juliamzfong@gmail.com>
2021-02-07 15:45:36 -0600

    Add completed problems

---
+++ MediumJS/findingNemo.js
@@ -0,0 +1,10 @@
+// You're given a string of words. You need to find the word "Nemo", 
+// and return a string like this: 
+// "I found Nemo at [the order of the word you find nemo]!".
+
+// If you can't find Nemo, return "I can't find Nemo :(".
+
+function findNemo(sentence) {
+    let str = str.sentence.split()
+  }
+  return findNemo
